145645f21c2e9d6a8a8b53d0e953efc6ec700909
dipens <dipenmshah8@gmail.com>
2021-02-07 15:00:06 -0500

    added semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react'
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div>
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/register' component={Register}></Route>
+      </Container>
+    </Router>
   );
 }
 
